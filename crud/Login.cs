b529e27d05f45c301c2dbd4fb23a542b07a8f090
Juan Andres Marin Cardenas <juan.marin133@pascualbravo.edu.co>
2024-09-02 14:39:03 -0500

    Reapply "Creacion de registro y login"
    This reverts commit d611ab0ea76f9716e5764c86335fbe0c5f2600d9.

--- crud/Login.cs
+++ crud/Login.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Datos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +18,49 @@ namespace Presentacion
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            string nombreUsuario = txtNombreUsuario.Text;
+            string contraseña = txtContraseña.Text;
+
+            // Consulta SQL para verificar las credenciales
+            string query = "SELECT * FROM Usuarios WHERE NombreUsuario = @NombreUsuario AND Contraseña = @Contraseña AND Activo = 1";
+            SqlParameter[] parameters = {
+        new SqlParameter("@NombreUsuario", nombreUsuario),
+        new SqlParameter("@Contraseña", contraseña)
+    };
+
+            DBConsultasLR db = new DBConsultasLR();
+            DataTable userTable = db.ExecuteQuery(query, parameters);
+
+            if (userTable.Rows.Count > 0)
+            {
+                MessageBox.Show("Login exitoso.");
+                // Aquí puedes abrir el formulario principal o continuar con la aplicación
+                this.Hide(); // Oculta el formulario de login
+                Menu formMenu = new Menu();
+                formMenu.Show();
+            }
+            else
+            {
+                MessageBox.Show("Nombre de usuario o contraseña incorrectos.");
+            }
+        }
+
+        private void btnRegistrarse_Click(object sender, EventArgs e)
+        {
+            // Crear una nueva instancia del formulario de registro
+            Register formRegister = new Register();
+
+            // Mostrar el formulario de registro
+            formRegister.Show();
+
+            // Opcional: Ocultar el formulario de login si deseas que solo uno esté visible a la vez
+            // this.Hide();
+        }
     }
 }
+
+
